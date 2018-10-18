174c5372ebdeb68db4b41aa003c501b96728c914
Ryohei Ueda <garaemon@gmail.com>
2018-10-19 02:16:00 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='insideout_downloader',
+    version='1.0.0',
+    url='https://github.com/garaemon/insideout_downloader.git',
+    author='garaemon',
+    author_email='garaemon@gmail.com',
+    description='Download insideout mp3 file',
+    packages=find_packages(),
+    install_requires=['beautifulsoup4'],
+    scripts=['bin/insideout_downloader', 'arrow', 'mutagen']
+)
